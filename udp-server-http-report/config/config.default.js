2ff866c884a1635bb14d1469876d77237af053ad
yewenhua <2574522520@qq.com>
2022-08-21 14:25:55 +0800

    bg

--- udp-server-http-report/config/config.default.js
+++ udp-server-http-report/config/config.default.js
@@ -153,6 +153,460 @@ module.exports = appInfo => {
         dir: path.join(__dirname, '../logs/eggjs')
     };
 
+    config.dcs = [
+        {
+            'itemCode': 'GL_LTSBWDT10',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRA_018L',  //测点在源系统的code
+            'cnName': '1#炉膛内上部断面左墙温度（T10）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTSBWDT10',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRA_018L',  //测点在源系统的code
+            'cnName': '2#炉膛内上部断面左墙温度（T10）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTSBWDT11',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRA_018M',  //测点在源系统的code
+            'cnName': '1#炉膛内上部断面前墙温度（T11）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTSBWDT11',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRA_018M',  //测点在源系统的code
+            'cnName': '2#炉膛内上部断面前墙温度（T11）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTSBWDT12',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRA_018R',  //测点在源系统的code
+            'cnName': '1#炉膛内上部断面右墙温度（T12）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTSBWDT12',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRA_018R',  //测点在源系统的code
+            'cnName': '2#炉膛内上部断面右墙温度（T12）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTZBWDT20',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRA_017L',  //测点在源系统的code
+            'cnName': '1#炉膛内中部断面左墙温度（T20）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTZBWDT20',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRA_017L',  //测点在源系统的code
+            'cnName': '2#炉膛内中部断面左墙温度（T20）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTZBWDT21',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRA_017M',  //测点在源系统的code
+            'cnName': '1#炉膛内中部断面前墙温度（T21）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTZBWDT21',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRA_017M',  //测点在源系统的code
+            'cnName': '2#炉膛内中部断面前墙温度（T21）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTZBWDT22',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRA_017R',  //测点在源系统的code
+            'cnName': '1#炉膛内中部断面右墙温度（T22）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTZBWDT22',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRA_017R',  //测点在源系统的code
+            'cnName': '2#炉膛内中部断面右墙温度（T22）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTXBWDT30',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRA_016L',  //测点在源系统的code
+            'cnName': '1#炉膛内下部断面左墙温度（T30）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTXBWDT30',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRA_016L',  //测点在源系统的code
+            'cnName': '2#炉膛内下部断面左墙温度（T30）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTXBWDT31',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRA_016M',  //测点在源系统的code
+            'cnName': '1#炉膛内下部断面前墙温度（T31）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTXBWDT31',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRA_016M',  //测点在源系统的code
+            'cnName': '2#炉膛内下部断面前墙温度（T31）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTXBWDT32',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRA_016R',  //测点在源系统的code
+            'cnName': '1#炉膛内下部断面右墙温度（T32）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTXBWDT32',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRA_016R',  //测点在源系统的code
+            'cnName': '2#炉膛内二次风喷入点温度',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTNECFPRDWD',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRA_015L',  //测点在源系统的code
+            'cnName': '1#炉膛内二次风喷入点温度',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTNECFPRDWD',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRA_015L',  //测点在源系统的code
+            'cnName': '2#炉膛内二次风喷入点温度',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTCKFY_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PRCA_019L',  //测点在源系统的code
+            'cnName': '1#炉膛出口负压(左侧)',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTCKFY_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PRCA_019L',  //测点在源系统的code
+            'cnName': '2#炉膛出口负压(左侧)',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTCKFY_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PRCA_019R',  //测点在源系统的code
+            'cnName': '1#炉膛出口负压(右侧)',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_LTCKFY_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PRCA_019R',  //测点在源系统的code
+            'cnName': '2#炉膛出口负压(右侧)',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_PHRQSW_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.LRCZA_001A',  //测点在源系统的code
+            'cnName': '1#炉汽包平衡容器水位(左侧)',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_PHRQSW_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.LRCZA_001A',  //测点在源系统的code
+            'cnName': '2#炉汽包平衡容器水位(左侧)',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_PHRQSW_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.LRCZA_001B',  //测点在源系统的code
+            'cnName': '1#炉汽包平衡容器水位(右侧)',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_PHRQSW_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.LRCZA_001B',  //测点在源系统的code
+            'cnName': '2#炉汽包平衡容器水位(右侧)',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_LQBYL',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PRCA_006A',  //测点在源系统的code
+            'cnName': '1#炉汽包压力',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_LQBYL',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PRCA_006A',  //测点在源系统的code
+            'cnName': '2#炉汽包压力',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_BW_ZS',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_033A',  //测点在源系统的code
+            'cnName': '1#汽包壁温（左上）',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_BW_ZS',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_033A',  //测点在源系统的code
+            'cnName': '2#汽包壁温（左上）',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_BW_YS',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_035A',  //测点在源系统的code
+            'cnName': '1#汽包壁温（右上）',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_BW_YS',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_035A',  //测点在源系统的code
+            'cnName': '2#汽包壁温（右上）',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_BW_ZX',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_033B',  //测点在源系统的code
+            'cnName': '1#汽包壁温（左下）',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_BW_ZX',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_033B',  //测点在源系统的code
+            'cnName': '2#汽包壁温（左下）',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_BW_YX',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_035B',  //测点在源系统的code
+            'cnName': '1#汽包壁温（右下）',  //测点中文名
+        },
+        {
+            'itemCode': 'LQB_BW_YX',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_035B',  //测点在源系统的code
+            'cnName': '2#汽包壁温（右下）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_ZZQWD',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRCA_010',  //测点在源系统的code
+            'cnName': '1#主蒸汽温度',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_ZZQWD',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRCA_010',  //测点在源系统的code
+            'cnName': '2#主蒸汽温度',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_ZZQYL',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PRA_010B',  //测点在源系统的code
+            'cnName': '1#主蒸汽压力',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_ZZQYL',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PRA_010B',  //测点在源系统的code
+            'cnName': '2#主蒸汽压力',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_ZZQLL',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.FRCQ_004COM',  //测点在源系统的code
+            'cnName': '1#主蒸汽流量',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_ZZQLL',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.FRCQ_004COM',  //测点在源系统的code
+            'cnName': '2#主蒸汽流量',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_XKPQFZT',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.HS_002_ZO',  //测点在源系统的code
+            'cnName': '1#向空排汽阀状态',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_XKPQFZT',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.HS_002_ZO',  //测点在源系统的code
+            'cnName': '2#向空排汽阀状态',  //测点中文名
+        },
+        {
+            'itemCode': 'GWGRQ_RKYQWD_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_021L',  //测点在源系统的code
+            'cnName': '1#高温过热器入口烟气温度（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GWGRQ_RKYQWD_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_021L',  //测点在源系统的code
+            'cnName': '2#高温过热器入口烟气温度（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GWGRQ_RKYQWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_021R',  //测点在源系统的code
+            'cnName': '1#高温过热器入口烟气温度（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GWGRQ_RKYQWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_021R',  //测点在源系统的code
+            'cnName': '2#高温过热器入口烟气温度（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GWGRQ_RKFY_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PI_021L',  //测点在源系统的code
+            'cnName': '1#高温过热器入口负压（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GWGRQ_RKFY_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PI_021L',  //测点在源系统的code
+            'cnName': '2#高温过热器入口负压（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GWGRQ_RKFY_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PI_021R',  //测点在源系统的code
+            'cnName': '1#高温过热器入口负压（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GWGRQ_RKFY_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PI_021R',  //测点在源系统的code
+            'cnName': '2#高温过热器入口负压（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'ZWGRQ_RKYQWD_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_022L',  //测点在源系统的code
+            'cnName': '1#中温过热器入口烟气温温（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'ZWGRQ_RKYQWD_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_022L',  //测点在源系统的code
+            'cnName': '2#中温过热器入口烟气温温（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'ZWGRQ_RKYQWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_022R',  //测点在源系统的code
+            'cnName': '1#中温过热器入口烟气温温（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'ZWGRQ_RKYQWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_022R',  //测点在源系统的code
+            'cnName': '2#中温过热器入口烟气温温（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'ZWGRQ_RKFY_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PI_022L',  //测点在源系统的code
+            'cnName': '1#中温过热器入口负压（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'ZWGRQ_RKFY_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PI_022L',  //测点在源系统的code
+            'cnName': '2#中温过热器入口负压（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'ZWGRQ_RKFY_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PI_022R',  //测点在源系统的code
+            'cnName': '1#中温过热器入口负压（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'ZWGRQ_RKFY_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PI_022R',  //测点在源系统的code
+            'cnName': '2#中温过热器入口负压（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'DWGRQ_RKYQWD_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_023L',  //测点在源系统的code
+            'cnName': '1#低温过热器入口烟气温度（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'DWGRQ_RKYQWD_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_023L',  //测点在源系统的code
+            'cnName': '2#低温过热器入口烟气温度（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'DWGRQ_RKYQWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_023R',  //测点在源系统的code
+            'cnName': '1#低温过热器入口烟气温度（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'DWGRQ_RKYQWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_023R',  //测点在源系统的code
+            'cnName': '2#低温过热器入口烟气温度（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'YJSMQ_RKYQWD_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_024L',  //测点在源系统的code
+            'cnName': '1#一级省煤器入口烟气温度（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'YJSMQ_RKYQWD_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_024L',  //测点在源系统的code
+            'cnName': '2#一级省煤器入口烟气温度（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'YJSMQ_RKYQWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_024R',  //测点在源系统的code
+            'cnName': '1#一级省煤器入口烟气温度（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'YJSMQ_RKYQWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_024R',  //测点在源系统的code
+            'cnName': '2#一级省煤器入口烟气温度（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'DWGRQ_RKYQWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PI_024L',  //测点在源系统的code
+            'cnName': '1#一级省煤器入口负压（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'YJSMQ_RKFY_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PI_024L',  //测点在源系统的code
+            'cnName': '2#一级省煤器入口负压（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'YJSMQ_RKFY_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PI_024R',  //测点在源系统的code
+            'cnName': '1#一级省煤器入口负压（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'YJSMQ_RKFY_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PI_024R',  //测点在源系统的code
+            'cnName': '2#一级省煤器入口负压（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_CKYHL_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.ARCA_001L',  //测点在源系统的code
+            'cnName': '1#锅炉出口氧含量（省煤器左侧出口氧量）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_CKYHL_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.ARCA_001L',  //测点在源系统的code
+            'cnName': '2#锅炉出口氧含量（省煤器左侧出口氧量）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_CKYHL_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.ARCA_001R',  //测点在源系统的code
+            'cnName': '1#锅炉出口氧含量（省煤器右侧出口氧量）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_CKYHL_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.ARCA_001R',  //测点在源系统的code
+            'cnName': '2#锅炉出口氧含量（省煤器右侧出口氧量）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_PYWD_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_025L',  //测点在源系统的code
+            'cnName': '1#排烟温度（省煤器出口左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_PYWD_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_025L',  //测点在源系统的code
+            'cnName': '2#排烟温度（省煤器出口左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_PYWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TR_025R',  //测点在源系统的code
+            'cnName': '1#排烟温度（省煤器出口右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_PYWD_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TR_025R',  //测点在源系统的code
+            'cnName': '2#排烟温度（省煤器出口右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_SMQCKFY_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PI_025L',  //测点在源系统的code
+            'cnName': '1#省煤器出口负压（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_SMQCKFY_L',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PI_025L',  //测点在源系统的code
+            'cnName': '2#省煤器出口负压（左侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_SMQCKFY_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PI_025R',  //测点在源系统的code
+            'cnName': '1#省煤器出口负压（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'GL_SMQCKFY_R',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PI_025R',  //测点在源系统的code
+            'cnName': '2#省煤器出口负压（右侧）',  //测点中文名
+        },
+        {
+            'itemCode': 'BDCCQ_RKYQWD',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.TRCA_052',  //测点在源系统的code
+            'cnName': '1#布袋除尘器入口烟气温度',  //测点中文名
+        },
+        {
+            'itemCode': 'BDCCQ_RKYQWD',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.TRCA_052',  //测点在源系统的code
+            'cnName': '2#布袋除尘器入口烟气温度',  //测点中文名
+        },
+        {
+            'itemCode': 'BDCCQ_YC',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine1.PDI_053',  //测点在源系统的code
+            'cnName': '1#布袋除尘器压差',  //测点中文名
+        },
+        {
+            'itemCode': 'BDCCQ_YC',  //测点在管控平台的CODE
+            'sourceCode': 'Applications.BurningLine2.PDI_053',  //测点在源系统的code
+            'cnName': '2#布袋除尘器压差',  //测点中文名
+        },
+        
+    ];
+
     return {
         ...config,
         ...userConfig,
