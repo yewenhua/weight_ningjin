0ab6203ba883224d9af22205fcd6fa85d4ea924b
yewenhua <2574522520@qq.com>
2022-08-21 10:17:59 +0800

    bg

--- node-tcp/MongoDb.js
+++ node-tcp/MongoDb.js
@@ -107,19 +107,22 @@ class DB {
             try {
                 collection.find(condition)
                     .project(column)
-                    .skip(limit * pages)
+                    .skip(limit * (pages - 1))
                     .limit(limit)
                     .toArray((err, docs) => {
                         if (err) {
+                            console.log('66666666666666');
                             reject(err);
                         } else {
+                            console.log('77777777777');
                             reslove(docs);
                         }
                     });
             } catch (error) {
+                console.log('88888888888888');
                 reject(error);
             }
-        }).catch(()=>{});
+        }).catch(()=>{console.log('999999999999');});
     }
 
     /**
